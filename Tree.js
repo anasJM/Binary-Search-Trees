c874f8fdecad7dcc79b04adc463af73b170f59f9
anasJM <anasjamlaoui@gmail.com>
2026-05-20 16:53:46 +0100

    includes() + insert() methods

--- Tree.js
+++ Tree.js
@@ -6,14 +6,45 @@ class Tree {
         this.root = this.#buildTree(this.array, 0, this.array.length - 1)
     }
 
+    // building the tree
     #buildTree(array, start, end) {
         if (start > end) return null
 
         let mid = Math.floor((start + end) / 2)
         let root = new Node(array[mid])
 
-        node.left = this.#buildTree(array, start, mid - 1)
-        node.left = this.#buildTree(array, mid + 1, end)
+        root.left = this.#buildTree(array, start, mid - 1)
+        root.right = this.#buildTree(array, mid + 1, end)
+
+        return root
+    }
+
+    // checks if the tree includes the given value
+    includes(value) {
+        let current = this.root
+
+        while (current !== null) {
+            if (value === current.data) return true
+
+            if (value < current.data) {
+                current = current.left
+            } else {
+                current = current.right
+            }
+        }
+
+        return false
+    }
+
+    // insert new value into the tree
+    insert(root, value) {
+        if (root === null)
+            return new Node(value)
+
+        if (value < root.data)
+            root.left = this.insert(root.left, value)
+        else
+            root.right = this.insert(root.right, value)
 
         return root
     }
---
+++ test.js
@@ -0,0 +1,20 @@
+import Tree from "./Tree.js";
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 100, 123]
+const tree = new Tree(array)
+
+// display the tree in console
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null || node === undefined) {
+        return;
+    }
+
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+}
+
+prettyPrint(tree.root)
+console.log(tree.includes(9)) // true
+tree.insert(tree.root, 500)
+prettyPrint(tree.root)
